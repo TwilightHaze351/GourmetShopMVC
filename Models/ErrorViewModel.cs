ae8626145756ee9c33c4b7850a59d9f5bd024060
TwilightHaze351 <145950319+TwilightHaze351@users.noreply.github.com>
2025-03-16 18:29:51 -0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GourmetShopMVCWebapp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
